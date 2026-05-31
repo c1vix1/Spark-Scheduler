baa1dbc5bdbb8256e139ef504733ebb816e5db63
cyu-12 <calvin.yu.12@outlook.com>
2026-05-31 17:11:35 -0500

    Added welcome screen

--- scheduler.py
+++ scheduler.py
@@ -1,10 +1,32 @@
 import os
+import json
 
-print("Spark Scheduler")
+debug = True
+exit = False
+
+with open("welcome.txt", "r", encoding="utf-8") as file:
+    print(file.read())
+
 try:
     # Query standard output descriptor
-    terminalColumns = os.get_terminal_size()[0]
-    print(f"Terminal width: {terminalColumns}")
+    terminalColumns, terminalLines = os.get_terminal_size()
+    if debug == True:
+        print(f"Terminal width: {terminalColumns}")
 except OSError:
     print("Not running in a standard terminal viewport.")
     terminalColumns = 10 
+for i in range(terminalColumns):
+    print("-",end="")
+
+if terminalLines <= 30 and debug == False:
+    print("Terminal Too Small. Please Raise Terminal Size")
+    exit = True
+
+while exit != True:
+    print("Hello")
+    exitResponce = input("Exit?(Y/N)").lower()
+    if exitResponce == "y" or exitResponce == "yes":
+        exit = True
+    else:
+        exit = False
+print("Exited")
