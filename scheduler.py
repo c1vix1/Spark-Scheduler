24ae37a036de11b53c133fa4ac5203042ae3a7a8
cyu-12 <calvin.yu.12@outlook.com>
2026-05-31 14:21:57 -0500

    Add way to find terminal size for <hr>

--- scheduler.py
+++ scheduler.py
@@ -1,1 +1,9 @@
+import os
+
 print("Spark Scheduler")
+try:
+    # Query standard output descriptor
+    terminalColumns, terminalsLines = os.get_terminal_size()
+    print(f"Terminal width: {terminalColumns}")
+except OSError:
+    print("Not running in a standard terminal viewport.")
