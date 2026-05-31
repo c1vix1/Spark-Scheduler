368e040d7c4c7386e3ac8561368ad26f378735a6
cyu-12 <calvin.yu.12@outlook.com>
2026-05-31 14:24:57 -0500

    Made changed method for terminalColumns

--- scheduler.py
+++ scheduler.py
@@ -3,7 +3,8 @@ import os
 print("Spark Scheduler")
 try:
     # Query standard output descriptor
-    terminalColumns, terminalsLines = os.get_terminal_size()
+    terminalColumns = os.get_terminal_size()[0]
     print(f"Terminal width: {terminalColumns}")
 except OSError:
     print("Not running in a standard terminal viewport.")
+    terminalColumns = 10 
