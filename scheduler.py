743d9c3425f7c28d10af36aeb1d0a5572d6cd8e5
cyu-12 <calvin.yu.12@outlook.com>
2026-06-04 10:24:11 -0500

    Added sqlite Db to store user events

--- scheduler.py
+++ scheduler.py
@@ -1,14 +1,19 @@
 import os
 import json
+import sqlite3
 
 debug = True
 exit = False
+
+con = sqlite3.connect("app_data.db")
+cur = con.cursor()
+
+cur.execute("CREATE TABLE IF NOT EXISTS events(name TEXT NOT NULL,type INTEGER NOT NULL,length FLOAT NOT NULL,preferedTime TIME,setTimeStart TIME,setTimeEnd TIME)")
 
 with open("welcome.txt", "r", encoding="utf-8") as file:
     print(file.read())
 
 try:
-    # Query standard output descriptor
     terminalColumns, terminalLines = os.get_terminal_size()
     if debug == True:
         print(f"Terminal width: {terminalColumns}")
@@ -18,15 +23,13 @@ except OSError:
 for i in range(terminalColumns):
     print("-",end="")
 
-if terminalLines <= 30 and debug == False:
+if terminalLines <= 15 and debug == False:
     print("Terminal Too Small. Please Raise Terminal Size")
     exit = True
 
 while exit != True:
-    print("Hello")
-    exitResponce = input("Exit?(Y/N)").lower()
-    if exitResponce == "y" or exitResponce == "yes":
-        exit = True
-    else:
-        exit = False
+    numberOfEvents = input("Total Events")
+    for i in range(numberOfEvents):
+        eventName = input("Event Name")
+
 print("Exited")
