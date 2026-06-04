fd2f233a9a7960c7e0e5055e3f02bf2e69a4034c
cyu-12 <calvin.yu.12@outlook.com>
2026-06-04 10:51:06 -0500

    Added DB file to .gitignore, added logic to add info to sqllite DB, Delete .json file

--- scheduler.py
+++ scheduler.py
@@ -8,7 +8,7 @@ exit = False
 con = sqlite3.connect("app_data.db")
 cur = con.cursor()
 
-cur.execute("CREATE TABLE IF NOT EXISTS events(name TEXT NOT NULL,type INTEGER NOT NULL,length FLOAT NOT NULL,preferedTime TIME,setTimeStart TIME,setTimeEnd TIME)")
+cur.execute("CREATE TABLE IF NOT EXISTS events(name TEXT NOT NULL,type INTEGER NOT NULL,length FLOAT NOT NULL,preferedTime TIME,setTimeStart TIME,setTimeEnd TIME);")
 
 with open("welcome.txt", "r", encoding="utf-8") as file:
     print(file.read())
@@ -22,14 +22,27 @@ except OSError:
     terminalColumns = 10 
 for i in range(terminalColumns):
     print("-",end="")
+print("\n")
 
 if terminalLines <= 15 and debug == False:
     print("Terminal Too Small. Please Raise Terminal Size")
     exit = True
 
 while exit != True:
-    numberOfEvents = input("Total Events")
+    numberOfEvents = int(input("Total Events"))
     for i in range(numberOfEvents):
-        eventName = input("Event Name")
+        eventName = input("Event Name:")
+        eventTypeStr = input("Event Type(use \"s\" for a Set Time Event, \"h\" for a High Priority Event, and \"n\" or leave blank for a Normal Priority Event)").lower()
+        if "s" in eventTypeStr:
+            eventType = 2
+        elif "h" in eventTypeStr:
+            eventType = 1
+        else: 
+            eventType = 0
+        eventLengthHour,eventLengthMinute = float(input("Event Length(Use \":\" to show minutes and hours using HH:MM)")).split(":")
+        eventLengthHour *= 60
+        eventLengthMinute += eventLengthHour
+        eventLength = eventLengthMinute
+        cur.execute(f"INSERT INTO events(name) VALUES(\"{eventName}\",{eventType},{eventLength});")
 
 print("Exited")
