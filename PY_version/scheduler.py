8f2433c1ea0cde925cfff2eaef9dd1083b6dca95
cyu-12 <calvin.yu.12@outlook.com>
2026-06-05 18:36:55 -0500

    Moved python version to make way for JS version in another folder

--- PY_version/scheduler.py
+++ PY_version/scheduler.py
@@ -1,18 +1,19 @@
 import os
 import json
 import sqlite3
-
+# Enviroment Options
 debug = True
 exit = False
-
+clearTable = True
+# create SQLite3 DB file and make cursor for navigation
 con = sqlite3.connect("app_data.db")
 cur = con.cursor()
-
+# SQLite3 Cursor create table to store event data
 cur.execute("CREATE TABLE IF NOT EXISTS events(name TEXT NOT NULL,type INTEGER NOT NULL,length FLOAT NOT NULL,preferedTime TIME,setTimeStart TIME,setTimeEnd TIME);")
-
+# Print welcome txt file
 with open("welcome.txt", "r", encoding="utf-8") as file:
     print(file.read())
-
+# Get terminal size for dashed <hr> and print <hr>
 try:
     terminalColumns, terminalLines = os.get_terminal_size()
     if debug == True:
@@ -22,13 +23,14 @@ except OSError:
     terminalColumns = 10 
 for i in range(terminalColumns):
     print("-",end="")
+# Add a newline to create space
 print("\n")
 
+# Alert if terminal is too small
 if terminalLines <= 15 and debug == False:
     print("Terminal Too Small. Please Raise Terminal Size")
     exit = True
-
-
+# Find total events
 numberOfEvents = int(input("Total Events"))
 for i in range(numberOfEvents):
     eventLength = 0 
@@ -55,8 +57,9 @@ for i in range(numberOfEvents):
                 INSERT INTO events(name,type,length) VALUES(\"{eventName}\",{eventType},{eventLength});
             """)
     con.commit()
-clearTable = bool(input("clearTable?"))
-if clearTable:
+print(str(clearTable)+" ClearTable")
+if clearTable == True:
     cur.execute("DELETE FROM events")
     con.commit()
+    print("Table Cleared")
 print("Exited")
