cae97e06504c745f0b0d14c9d73d63cf52531481
cyu-12 <calvin.yu.12@outlook.com>
2026-06-09 21:04:39 -0500

    dfghjk

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ var totalEvents = 10;
 const table = document.getElementById("schedulerTable");
 const runButton = document.getElementById("runButton");
 
-runButton.addEventListener("click", addRows(totalEvents));
+runButton.addEventListener("click", addRows);
     
 
-function addRows(numOfRows) {
-    for (let i = 0; i < numOfRows; i++) {
+function addRows(e) {
+    console.log(e.key)
+    console.log("adding Rows")
+    console.log(totalEvents)
+    for (let i = 0; i < totalEvents; i++) {
         let row = table.insertRow(-1);
         let cell0 = row.insertCell(0);
         let ce1l1 = row.insertCell(1);
